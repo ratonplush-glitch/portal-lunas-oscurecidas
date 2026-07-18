11737eb9cdb6be28771599e5f3ff146df6e9d387
ECAR PRO <admin@ecarsac.com>
2026-07-17 19:26:17 -0500

    Corregir conexión MySQL Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const conexion = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "portal_lunas"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 conexion.connect((err) => {
